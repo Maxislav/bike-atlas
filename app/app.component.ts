b87f87a30187468b6e7e17a75e777cfba3d26a6a
Maxislav <gliderman2>
2016-03-06 00:08:43 +0200

    mymapEvents service

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,6 @@
 import {Component, View} from 'angular2/core';
 import {MyMap} from './mymap'
 import {FooterHelp} from './footer.help'
-import {LatLngService} from "./services/service.lat.lng";
 
 @Component({
     selector: 'my-app'
