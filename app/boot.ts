5a7513c91f0b432d20c5341ce2706cc7cd99fa59
Maxim Lipatov <lmaxim@mail.ru>
2016-08-04 11:00:57 +0300

    init dev 4.08.16

--- app/boot.ts
+++
@@ -1,8 +0,0 @@
-import {bootstrap}    from 'angular2/platform/browser'
-import {AppComponent} from './app.component'
-import {MymapEvents} from "./services/service.map.events";
-import {LocalStorage} from "../lib/a2/local_storage";
-import {ServiceMenu} from "./services/service.menu";
-
- var appPromise = bootstrap(AppComponent, [  MymapEvents , ServiceMenu]);
-
