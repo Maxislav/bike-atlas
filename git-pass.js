dba0e5541d1dc658ef30274519cfd9630bc09fa5
Maxim Lipatov <lmaxim@mail.ru>
2016-08-10 12:56:57 +0300

    olo

--- git-pass.js
+++ git-pass.js
@@ -2,11 +2,36 @@
  * Created by maxislav on 10.08.16.
  */
 const exec = require('child_process').exec;
-exec("zip -P \"g\" maptoken.zip -r maptoken.json", (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-  console.log(`stderr: ${stderr}`);
-});
+
+toZip()
+  .then(toCommit);
+
+function toZip() {
+  return new Promise((resolve, reject)=>{
+    exec("zip -P \"g\" maptoken.zip -r maptoken.json", {timeout:1000}, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        reject(error);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+      console.log(`stderr: ${stderr}`);
+      resolve('ok')
+    });
+  })
+}
+function toCommit() {
+  return new Promise((resolve, reject)=>{
+    exec("git add && git commit -m \"commit new zip pass\"", {timeout:1000}, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        reject(error);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+      console.log(`stderr: ${stderr}`);
+      resolve('ok')
+    });
+  })
+}
+
