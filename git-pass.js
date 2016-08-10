610c2cf994edaff94f4035a41cea54955f94c0f3
Maxim Lipatov <lmaxim@mail.ru>
2016-08-10 13:19:45 +0300

    commit new zip pass

--- git-pass.js
+++ git-pass.js
@@ -2,13 +2,44 @@
  * Created by maxislav on 10.08.16.
  */
 const exec = require('child_process').exec;
-
-toZip()
+var fs = require('fs');
+getPass()
+  .then(toZip)
   .then(toCommit);
 
-function toZip() {
+//toZip()
+  //.then(toCommit);
+
+
+function getPass() {
   return new Promise((resolve, reject)=>{
-    exec("zip -P \"g\" maptoken.zip -r maptoken.json", {timeout:1000}, (error, stdout, stderr) => {
+
+    fs.readFile('user-pass.json', 'utf8', function(err, contents) {
+      if(err){
+        console.log(err);
+        reject();
+        return
+      }
+      
+      let json, pass;
+      
+      try{
+        json = JSON.parse(contents)
+      }catch (err){
+        console.log(err);
+        reject();
+      }
+      
+      console.log(json.pass);
+      resolve(json.pass)
+    });
+  })
+}
+
+
+function toZip(pass) {
+  return new Promise((resolve, reject)=>{
+    exec("zip -P \""+pass+"\" maptoken.zip -r maptoken.json", {timeout:1000}, (error, stdout, stderr) => {
       if (error) {
         console.error(`exec error: ${error}`);
         reject(error);
