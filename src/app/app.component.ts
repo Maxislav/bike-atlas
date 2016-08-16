74d4b816642d43ab3756477d24478b6fee7376b6
Maxim Lipatov <lmaxim@mail.ru>
2016-08-16 10:57:50 +0300

    2.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,12 @@ import { Component } from '@angular/core';
     templateUrl: 'src/app/template/my-app.html'
 })
 
+
 export class AppComponent {
     title = 'Tour of Heroes';
+
+    selectedHero: Hero;
+    
     hero: Hero = {
         id: 1,
         name : 'Ololo'
@@ -16,4 +20,16 @@ export class AppComponent {
 class Hero{
     id: number;
     name: string
-};
+}
+const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
