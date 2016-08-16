ffd35a4ff1bf70264a40e8a60e71e191a45e671a
Maxim Lipatov <lmaxim@mail.ru>
2016-08-16 10:40:34 +0300

    2.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,19 @@
 import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    //template: '<h1>My First Angular 2 App</h1>'
+    templateUrl: 'src/app/template/my-app.html'
 })
-export class AppComponent { }
+
+export class AppComponent {
+    title = 'Tour of Heroes';
+    hero: Hero = {
+        id: 1,
+        name : 'Ololo'
+    };
+}
+
+class Hero{
+    id: number;
+    name: string
+};
