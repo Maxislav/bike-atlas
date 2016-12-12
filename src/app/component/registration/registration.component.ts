55e6bc1014c5b2ad8a1bd91d305333cdf2aca137
Maxim Lipatov <lmaxim@mail.ru>
2016-12-12 19:40:29 +0200

    registration state

--- src/app/component/registration/registration.component.ts
+++ src/app/component/registration/registration.component.ts
@@ -1,8 +1,18 @@
 import {Component} from '@angular/core';
+import {Location} from '@angular/common';
+
 @Component({
     moduleId: module.id,
-    template: '<div>Регг</div>'
+    templateUrl: './registration.component.html',
+    styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent{
-
+    constructor(private location: Location) {
+    }
+    onCancel(){
+        this.location.back();
+    }
+    onOk(){
+        this.location.back();
+    }
 }
