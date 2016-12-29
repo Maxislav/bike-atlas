442e6c8f1c315bfb3f5e60a721a7c6990303d0c4
Maxim Lipatov <lmaxim@mail.ru>
2016-12-29 17:53:18 +0200

    regist small edit

--- server/socket-data.js
+++ server/socket-data.js
@@ -9,68 +9,64 @@ const connection = mysql.createConnection(config.mysql);
 
 
 /**
- * Ghjверка ли существукт user
+ * Проверка ли существукт user
  * @param arrData
  * @returns {Promise}
  */
 
 function checkExistUser(arrData) {
 
-    return new Promise((resolve, reject) => {
-        const query = 'SELECT `name` from user WHERE `name`=? order by `id` desc limit 150';
-        connection.query(query, arrData, (err, rows) => {
-            if (err) {
-                reject(err);
-                return
-            }
-            console.log('Rowx =>', rows)
-            resolve(rows)
-        })
+  return new Promise((resolve, reject) => {
+    const query = 'SELECT `name` from user WHERE `name`=? order by `id` desc limit 150';
+    connection.query(query, arrData, (err, rows) => {
+      if (err) {
+        reject(err);
+        return
+      }
+      resolve(rows)
     })
+  })
 }
 
 function addUser(arrData) {
-    return new Promise((resolve, reject) => {
-        connection.query('INSERT INTO `user` (`id`, `name`, `pass`, `opt`) VALUES (NULL, ?, ?, NULL)', arrData, (err, results) => {
-            if (err) {
-                reject(err);
-                return;
-            }
-
-
-            //resolve(results)
-            resolve({
-                result: 'ok',
-                message: null
-            })
-        })
+  return new Promise((resolve, reject) => {
+    connection.query('INSERT INTO `user` (`id`, `name`, `pass`, `opt`) VALUES (NULL, ?, ?, NULL)', arrData, (err, results) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve({
+        result: 'ok',
+        message: null
+      })
     })
+  })
 }
 
 
 function onRegist(data) {
-    const tepmlate = ['name', 'pass'];
-    const arrData = [];
-    tepmlate.forEach(item => {
-        arrData.push(data[item])
-    });
+  const tepmlate = ['name', 'pass'];
+  const arrData = [];
+  tepmlate.forEach(item => {
+    arrData.push(data[item])
+  });
 
 
-    return checkExistUser(arrData)
-        .then((rows) => {
-            if (rows.length) {
-                return {
-                    result: false,
-                    message: 'User exist'
-                }
-            } else {
-                return addUser(arrData)
-            }
+  return checkExistUser(arrData)
+    .then((rows) => {
+      if (rows.length) {
+        return {
+          result: false,
+          message: 'User exist'
+        }
+      } else {
+        return addUser(arrData)
+      }
 
-        })
-        .catch((err) => {
-            console.log('onRegist +>', err)
-        })
+    })
+    .catch((err) => {
+      console.log('onRegist +>', err)
+    })
 
 
 }
@@ -78,36 +74,41 @@ function onRegist(data) {
 
 module.exports = (sever) => {
 
-    io(sever).on('connection', function (socket) {
-        socket.emit('news', {hello: 'world'});
-        socket.on('my other event', function (data) {
-            console.log(data);
-        });
+  io(sever).on('connection', function (socket) {
+    socket.emit('news', {hello: 'world'});
+    socket.on('my other event', function (data) {
+      console.log(data);
+    });
 
-        socket.on('onRegist', (d) => {
-            console.log(d)
+    socket.on('onRegist', (d) => {
+      console.log('onRegist start', d);
 
-            onRegist(d)
-                .then(d => {
-                    socket.emit('onRegist', d)
-                }, err => {
-                    console.error(err)
-                })
-        });
+      onRegist(d)
+        .then(d => {
+          socket.emit('onRegist', d)
+        }, err => {
+          console.error(err)
+        })
+        .catch((err)=> {
+          console.error('Cache onRegist', err);
+          socket.emit('onRegist', {result: false, status: 500, message: err})
+        })
 
-
-        socketStream(socket).on('file', function (stream) {
-            let data = [];
-            stream.on('data', (d) => {
-                data.push(d);
-            });
-            stream.on('end', (e, d) => {
-                console.log("file send")
-                socket.emit('file', Buffer.concat(data));
-            });
-        });
     });
 
 
-    //INSERT INTO `user` (`id`, `name`, `pass`, `opt`) VALUES (NULL, 'max', 'eeew', NULL);
+    socketStream(socket).on('file', function (stream) {
+      let data = [];
+      stream.on('data', (d) => {
+        data.push(d);
+      });
+      stream.on('end', (e, d) => {
+        console.log("file send")
+        socket.emit('file', Buffer.concat(data));
+      });
+    });
+  });
+
+
+  //INSERT INTO `user` (`id`, `name`, `pass`, `opt`) VALUES (NULL, 'max', 'eeew', NULL);
 };
