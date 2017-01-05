cfa0fe9e704a976f451549cd01cd9cb7b961b30c
Maxislav <lmaxim@mail.ru>
2017-01-05 12:57:22 +0200

    profile process

--- dist/app/component/profile/profile.component.js
+++ dist/app/component/profile/profile.component.js
@@ -10,12 +10,34 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require("@angular/core");
 var common_1 = require('@angular/common');
+var auth_service_1 = require("../../service/auth.service");
 var ProfileComponent = (function () {
-    function ProfileComponent(location) {
+    function ProfileComponent(location, elRef, as) {
         this.location = location;
+        this.elRef = elRef;
+        this.imageurl = 'src/img/no-avatar.gif';
+        this.name = as.userName;
     }
+    ProfileComponent.prototype.ngAfterViewInit = function () {
+        var _this = this;
+        var el = this.elRef.nativeElement;
+        var inputEl = this.inputEl = el.getElementsByTagName("input")[0];
+        inputEl.addEventListener('change', function () {
+            console.log(inputEl.files);
+            var file = inputEl.files[0];
+            var reader = new FileReader();
+            reader.onload = function (event) {
+                var the_url = event.target.result;
+                _this.imageurl = the_url;
+            };
+            reader.readAsDataURL(file);
+        });
+    };
     ProfileComponent.prototype.onClose = function () {
         this.location.back();
+    };
+    ProfileComponent.prototype.onOpen = function () {
+        this.inputEl.click();
     };
     ProfileComponent = __decorate([
         core_1.Component({
@@ -23,7 +45,7 @@ var ProfileComponent = (function () {
             templateUrl: './profile.component.html',
             styleUrls: ['./profile.component.css'],
         }), 
-        __metadata('design:paramtypes', [common_1.Location])
+        __metadata('design:paramtypes', [common_1.Location, core_1.ElementRef, auth_service_1.AuthService])
     ], ProfileComponent);
     return ProfileComponent;
 }());
--- src/app/component/profile/profile.component.ts
+++ src/app/component/profile/profile.component.ts
@@ -1,15 +1,43 @@
-import {Component} from "@angular/core";
+import {Component, ElementRef, AfterViewInit} from "@angular/core";
 import {Location} from '@angular/common';
+import {AuthService} from "../../service/auth.service";
+
+
+
 @Component({
     moduleId: module.id,
     templateUrl: './profile.component.html',
     styleUrls: ['./profile.component.css'],
 })
-export class ProfileComponent{
-    constructor(private location: Location){
+export class ProfileComponent implements AfterViewInit{
+    private imageurl: string;
+    private inputEl: Element;
+    private name: string;
 
+    ngAfterViewInit():void{
+        const el =this.elRef.nativeElement;
+        const inputEl = this.inputEl = el.getElementsByTagName("input")[0];
+        inputEl.addEventListener('change', ()=>{
+            console.log(inputEl.files);
+            const file = inputEl.files[0];
+            const reader = new FileReader();
+            reader.onload = (event) => {
+                const the_url = event.target.result;
+                this.imageurl =the_url
+            };
+            reader.readAsDataURL(file);
+        });
+    }
+    constructor(private location: Location, private elRef: ElementRef, as: AuthService){
+        this.imageurl = 'src/img/no-avatar.gif';
+        this.name = as.userName;
     }
     onClose(){
         this.location.back()
     }
+    onOpen(){
+        this.inputEl.click()
+    }
+
 }
+
