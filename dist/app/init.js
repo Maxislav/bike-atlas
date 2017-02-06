e1db79cf496348e6cf2adc4f923dda583342f0ab
Maxim Lipatov <lmaxim@mail.ru>
2017-02-06 17:18:43 +0200

    css preload

--- dist/app/init.js
+++ dist/app/init.js
@@ -1,7 +1,5 @@
 "use strict";
-const platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-const app_module_1 = require('./app.module');
 const core_1 = require('@angular/core');
 core_1.enableProdMode();
-platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
+//platformBrowserDynamic().bootstrapModule(AppModule); 
 //# sourceMappingURL=init.js.map
