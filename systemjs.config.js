66dd100c1c44661b7b3180e7699350c32e6b7a0e
Maxim Lipatov <lmaxim@mail.ru>
2017-02-22 10:25:15 +0200

    translate start

--- systemjs.config.js
+++ systemjs.config.js
@@ -14,30 +14,11 @@
     'socket':                     'node_modules/socket.io-client/dist',
     'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
     'rxjs':                       'node_modules/rxjs',
-      'css': 'node_modules/systemjs-plugin-css/css.js'
-    /*'rxjs/Subject':                       'dist/rx.min.js',
-    'rxjs/Observable':                       'dist/rx.min.js',
-    'rxjs/observable/PromiseObservable':                       'dist/rx.min.js',
-    'rxjs/operator/toPromise':                       'dist/rx.min.js',
-    '/rxjs/observable/from ' : 'dist/rx.min.js' ,
-    '/rxjs/operator/every ' : 'dist/rx.min.js' ,
-    '/rxjs/observable/of ' : 'dist/rx.min.js' ,
-    '/rxjs/operator/map ' : 'dist/rx.min.js' ,
-    '/rxjs/operator/mergeMap ' : 'dist/rx.min.js' ,
-    '/rxjs/operator/mergeAll ' : 'dist/rx.min.js' ,
-    '/rxjs/operator/reduce' : 'dist/rx.min.js' ,
-    '/rxjs/operator/catch ' : 'dist/rx.min.js' ,
-    '/rxjs/operator/concatAll ' : 'dist/rx.min.js' ,
-    '/rxjs/operator/first' : 'dist/rx.min.js' ,
-    '/rxjs/util/EmptyError' : 'dist/rx.min.js' ,
-    '/rxjs/observable/fromPromise' : 'dist/rx.min.js' ,
-    '/rxjs/operator/last' : 'dist/rx.min.js' ,
-    '/rxjs/BehaviorSubject' : 'dist/rx.min.js'*/
+    'css': 'node_modules/systemjs-plugin-css/css.js'
   };
-  // packages tells the System loader how to load when no filename and/or no extension
   var packages = {
     'app':                        { main: 'init.js',  defaultExtension: 'js' },
-    'ng2-translate':                        { main: 'bundles/ng2-translate.umd.js',  defaultExtension: 'js' },
+    'ng2-translate':              { main: 'bundles/ng2-translate.umd.js',  defaultExtension: 'js' },
     'rxjs':                       { main : 'Rx.js', defaultExtension: 'js' },
     'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' }
   };
@@ -57,14 +38,12 @@
   // Individual files (~300 requests):
   function packIndex(pkgName) {
     packages['@angular/'+pkgName] = { main: 'index.js', defaultExtension: 'js' };
-    packages['@ngx-translate/'+pkgName] = { main: 'index.js', defaultExtension: 'js' };
     packages['ng2-translate/'+pkgName] = { main: 'index.js', defaultExtension: 'js' };
   }
   // Bundled (~40 requests):
   function packUmd(pkgName) {
     packages['@angular/'+pkgName] = { main: '/bundles/' + pkgName + '.umd.min.js', defaultExtension: 'js' };
     packages['ng2-translate/'+pkgName] = { main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
-    packages['@ngx-translate/'+pkgName] = { main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
 
   }
   // Most environments should use UMD; some (Karma) need the individual index files
