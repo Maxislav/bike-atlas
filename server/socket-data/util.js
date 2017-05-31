3b2270cfb8e96c69de1e5d01c4392aa1666eebbb
Maxim Lipatov <lmaxim@mail.ru>
2017-05-31 10:56:24 +0300

    Update track

--- server/socket-data/util.js
+++ server/socket-data/util.js
@@ -830,35 +830,45 @@ class Util {
 		})
 	}
 	downloadTrack(deviceKey, point){
-
 		return this.checkExistPoint(deviceKey, point.lng, point.date)
 			.then(res=>{
 				if(res){
-					return Promise.reject('point exist')
-				}else {
-					return new Promise((resolve, reject) => {
-						this.connection.query('INSERT INTO `logger` ' +
-							'(`id`, `device_key`, `lng`, `lat`, `speed`, `date`) VALUES (NULL, ?, ?, ?, ?, ?)',
-							[deviceKey,  point.lng, point.lat, point.speed, new Date(point.date)], (err, results) => {
-								if(err){
-									reject(err); return;
-								}
-								resolve(results)
-							})
+					return new Promise((resolve, reject) =>{
+						this.connection.query('DELETE FROM `logger` WHERE `id`=?', [res.id], (err, result) => {
+							if (err) {
+								reject(err);
+								return;
+							}
+							resolve(true)
+						})
 					})
+				}else{
+					return Promise.resolve(true)
 				}
-			});
+			})
+			.then(d=>{
+				return new Promise((resolve, reject) => {
+					this.connection.query('INSERT INTO `logger` ' +
+						'(`id`, `device_key`, `lng`, `lat`, `speed`, `date`) VALUES (NULL, ?, ?, ?, ?, ?)',
+						[deviceKey,  point.lng, point.lat, point.speed, new Date(point.date)], (err, results) => {
+							if(err){
+								reject(err); return;
+							}
+							resolve(results)
+						})
+				})
+			})
 	}
 	checkExistPoint(deviceKey, lng, date){
 
 		return new Promise((resolve, reject) => {
 			this.connection.query('SELECT * FROM `logger` WHERE `device_key`=? AND date=?', [deviceKey, new Date(date)], function (err, rows) {
 				if(err){
-					reject(err)
+					reject(err);
 					return;
 				}
 				if(rows && rows.length){
-					resolve(true)
+					resolve(rows)
 				}else{
 					resolve(false)
 				}
