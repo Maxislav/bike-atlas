31d56f1b1ba2d9a5b3fc2d1572b724c1648826a4
maksim.lipatov <Maksim.Lipatov@ihsmarkit.com>
2019-06-06 14:35:24 +0300

    parse unic date for dima

--- server/logger.js
+++ server/logger.js
@@ -12,7 +12,8 @@ class Logger {
 // $GPRMC,074553,A,5006.02390,N,3033.30500,E,16.895118,220.089996,290718,,*1A   -> 50.10039902
 
     //  /log?id=862614000171302&dev=862614000171302&acct=862614000171302&batt=0&code=0xF020&alt=0.0&gprmc=$GPRMC,111925,A,5023.32022,N,3029.64240,E,0.000000,0.000000,050117,,*29
-    //  /log?id=862614000171302&dev=862614000171302&acct=862614000171302&batt=0&code=0xF020&alt=0.0&gprmc=$GPRMC,152524,A,5005.91360,N,3033.15540,E,13.386224,222.130005,010818,,*1E
+    //  /log?id=862614000171302 &dev=862614000171302&acct=862614000171302&batt=0&code=0xF020&alt=0.0&gprmc=$GPRMC,152524,A,5005.91360,N,3033.15540,E,13.386224,222.130005,010818,,*1E
+    //  /log?id=222222222222&dev=222222222222&acct=222222222222&batt=0&code=0xF020&alt=0.0&gprmc=$GPRMC,043137,A,5026.94750,N,3024.56420,E,0.000000,216.429993,150963,,*20
     constructor(_app, _ioServer, util) {
         app = _app;
         ioServer = _ioServer;
@@ -28,7 +29,8 @@ class Logger {
     }
 
     onLog(req, res, next) {
-        const util = this.util
+
+        const util = this.util;
 
         const device_id = req.query.id;
         let data = null;
@@ -150,7 +152,7 @@ class Logger {
         //console.log('logger onDisconnect ->', this.devices)
     }
 
-    // $GPRMC,153946,A,5023.31220,N,3029.63150,E,0.000000,0.000000,03 01 17,,*2A
+    // $GPRMC,153946,A,5023.31220,N,3029.63150,E,0.000000,0.000000,030117,,*2A
 
 
     //$GPRMC,030853,A,5026.98660,N,3024.51060,E,2.798506,109.540001, 15 09 63   ,,*20
@@ -166,12 +168,16 @@ class Logger {
         if(id === '222222222222'){
             date = new Date(
                 year,
-                Number('' + dateStamp[2] + dateStamp[3]) - 3, //month
-                Number('' + dateStamp[0] + dateStamp[1]) -10, //day
+                Number('' + dateStamp[2] + dateStamp[3]) - 4, //month
+                Number('' + dateStamp[0] + dateStamp[1]) - 9, //day
                 Number( '' + timeStamp[0] + timeStamp[1]) + 6, //hour
                 Number('' + timeStamp[2] + timeStamp[3]) + 30, //min
-                Number('' + timeStamp[4] + timeStamp[5]) + 30, //sec
-            )
+                Number('' + timeStamp[4] + timeStamp[5]) , //sec
+            );
+            lat = arrData[4] === 'N' ? minToDec(arrData[3]) : '-' + minToDec(arrData[3]);
+            lng = arrData[6] === 'E' ? minToDec(arrData[5]) : '-' + minToDec(arrData[5]);
+            azimuth = parseFloat(Number(arrData[8]).toFixed(2));
+            speed = parseFloat(arrData[7]) * 1.852;
         }else{
             date = new Date(
                 year,
