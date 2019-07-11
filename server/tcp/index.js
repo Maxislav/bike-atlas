f9b9d06854b653b2b4b49d29090693afde60d261
maksim.lipatov <Maksim.Lipatov@ihsmarkit.com>
2019-07-11 14:03:51 +0300

    tcp server

---
+++ server/tcp/index.js
@@ -0,0 +1,23 @@
+const net = require('net');
+const streams = [];
+
+var server = net.createServer((c) => {
+    console.log('connect');
+    streams.push(c);
+    c.on('end', () => {
+        console.log('client disconnected');
+        const index = streams.indexOf(c);
+        if(-1<index){
+            streams.splice(index,1);
+        }
+    });
+    c.on('data', function (onStreamData)  {
+        console.log(onStreamData.toString())
+        //c.write(`Echo server\r\n ${onStreamData.toString()}`);
+        //c.end()
+    });
+});
+
+
+
+server.listen(8084);
