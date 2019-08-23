8318ba3314ea86f804f8433827c2e7e478bf11e1
maksim.lipatov <Maksim.Lipatov@ihsmarkit.com>
2019-08-23 20:14:17 +0300

    deep copy add

---
+++ server/util/deep-copy.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deepCopy = (oldObj) => {
+    var newObj = oldObj;
+    if (oldObj && typeof oldObj === "object") {
+        newObj = Object.prototype.toString.call(oldObj) === "[object Array]" ? [] : {};
+        for (var i in oldObj) {
+            newObj[i] = this.deepCopy(oldObj[i]);
+        }
+    }
+    return newObj;
+};
+//# sourceMappingURL=deep-copy.js.map
