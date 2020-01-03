392bfe42a903273bd54ba0157e11d293a21ba314
maksim.lipatov <Maksim.Lipatov@ihsmarkit.com>
2020-01-03 13:07:09 +0200

    Hmr add

--- src/app/init.ts
+++ src/app/init.ts
@@ -1,3 +1,4 @@
+/*
 
 import {enableProdMode} from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
@@ -7,3 +8,30 @@ import { AppModule } from './app.module';
 enableProdMode();
 platformBrowserDynamic().bootstrapModule(AppModule)
     .catch(err => console.error(err));
+
+*/
+
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+import { environment } from '../environments/environment';
+import { hmrBootstrap } from '../hmr';
+
+if (environment.production) {
+    enableProdMode();
+}
+
+const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule);
+
+declare const module: any;
+
+if (environment.hmr) {
+    if (module['hot']) {
+        hmrBootstrap(module, bootstrap);
+    } else {
+        console.error('HMR is not enabled for webpack-dev-server!');
+        console.log('Are you using the --hmr flag for ng serve?');
+    }
+} else {
+    bootstrap().catch(err => console.log(err));
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: true,
     //TODO set to prod
-    hostPrefix: 'http://178.62.44.54:8080/'
+    hostPrefix: 'http://178.62.44.54:8080/',
+    hmr: false
     //hostPrefix: ''
 };
