394a40a2aae915a21189261653311373aabc07be
Maksym Lipatov <maksym_lipatov@epam.com>
2026-01-22 13:30:28 +0200

    migration 15 ok

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,25 +1,24 @@
 /**
  * Created by maxislav on 05.10.16.
  */
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { AuthComponent } from './component/auth-component/auth.component';
-import { MapComponent } from './map.component';
-import { RegistrationComponent } from './component/registration/registration.component';
-import { DeviceComponent } from './component/device/device.component';
-import { AuthService } from './service/auth.service';
-import { ProfileComponent } from './component/profile/profile.component';
-import { JournalComponent, LeafletResolver } from './component/journal-component/journal.component';
-import { FriendsComponent } from './component/friends-component/friends-component';
-import { PrivateArea } from './component/private-area/private-area';
-import { NoFoundComponent } from './no-found.component';
-import { AllUserComponent } from './component/all-user/all-user.component';
-import { StravaComponent } from './component/strava-component/strava-component';
-import { StravaAuthComponent } from './component/strava-component/strava-auth-component';
-import { GtgbcComponent } from './component/gtgbc/gtgbc.component';
-import { DeviceHelpComponent } from './component/device/device-help/device-help.component';
-import { OneUserComponent } from 'src/app/component/all-user/one-user/one-user.component';
-import {MapService} from './service/map.service';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {AuthComponent} from './component/auth-component/auth.component';
+import {MapComponent} from './map.component';
+import {RegistrationComponent} from './component/registration/registration.component';
+import {DeviceComponent} from './component/device/device.component';
+import {AuthService} from './service/auth.service';
+import {ProfileComponent} from './component/profile/profile.component';
+import {JournalComponent, LeafletResolver} from './component/journal-component/journal.component';
+import {FriendsComponent} from './component/friends-component/friends-component';
+import {PrivateArea} from './component/private-area/private-area';
+import {NoFoundComponent} from './no-found.component';
+import {AllUserComponent} from './component/all-user/all-user.component';
+import {StravaComponent} from './component/strava-component/strava-component';
+import {StravaAuthComponent} from './component/strava-component/strava-auth-component';
+import {GtgbcComponent} from './component/gtgbc/gtgbc.component';
+import {DeviceHelpComponent} from './component/device/device-help/device-help.component';
+import {OneUserComponent} from 'src/app/component/all-user/one-user/one-user.component';
 
 const appRouters: Routes = [
     {
@@ -113,33 +112,11 @@ const appRouters: Routes = [
         redirectTo: '404',
     },
 
-    /*{
-        path: 'dashboard',
-        component: DashboardComponent,
-        children: [
-            {
-                path:''
-            },
-            {
-                path:'k',
-                component: HeroDetailComponent,
-                resolve:  {
-                    transactions :TransactionResolver
-                }
-
-            }
-
-        ]
-
-    }*/
 ];
 
 
-//export const MyRouterModule: ModuleWithProviders = RouterModule.forRoot(appRouters, {useHash: true});
-
-
 @NgModule({
-    imports: [RouterModule.forRoot(appRouters, { useHash: true, relativeLinkResolution: 'legacy' })],
+    imports: [RouterModule.forRoot(appRouters, {useHash: true})],
     exports: [RouterModule]
 })
 export class MyRouterModule {
